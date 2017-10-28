f8080a3f3a43da8e5b42bb936dbb09f07ef3658c
Vingt-2 <vingt-2@hotmail.fr>
2017-10-27 20:32:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, Extension
 pybvh_module = Extension('PyBVH.cSkeletalMotion', sources = ['src/pybvh_module.cpp','src/skeletalMotion.cpp','src/bvh_importer.cpp' ], include_dirs = ['./include'], library_dirs = [], libraries = [])
 
 setup (name = 'PyBVH',
-        version = '1.0',
+        version = '0.1',
         description = 'BVH loader for Python',
         packages = [ 'PyBVH' ],
         package_dir = { '' : 'src' },
